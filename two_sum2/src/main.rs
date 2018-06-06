4572db0e1e6d571cc08a2e56d53ee63c358e0d3e
Adrian Taylor <adrian@macrobug.com>
2018-06-05 23:25:28 -0700

    Better solution

--- two_sum2/src/main.rs
+++ two_sum2/src/main.rs
@@ -1,23 +1,30 @@
+use std::collections::HashMap;
+
 fn main() {
-	let inputs= [2, 7, 11, 15];
-	let target= 18;
-	let result= two_sum(&inputs, target);
-	println!("result: {}, {}", result.0, result.1);
+  let inputs= [2, 7, 11, 15];
+  let target= 18;
+  let result= two_sum(&inputs, target);
+  println!("result: {}, {}", result.0, result.1);
 }
 
 fn two_sum(inputs: &[i32], target: i32) -> (usize, usize) {
-  // Start with naïve approach
-	for first in 0..inputs.len(){
-		for second in 0..inputs.len(){
-			if first == second {
-				continue;
-			}
-		  if target - inputs[first] == inputs[second] {
-			  return (first, second)
-		  }
-		}
-	}
-	panic!("No match found");
+  let mut positions = HashMap::new();
+  // O(n)
+  for pos in 0..inputs.len() {
+    positions.insert(inputs[pos], pos);
+  }
+  // O(n)
+  for pos in 0..inputs.len() {
+    let desired = target - inputs[pos];
+    match positions.get(&desired) {
+      None => continue,
+      Some(second_pos) => return (pos, *second_pos)
+    };
+  }
+  // Could do this second loop in O(log n) using binary
+  // search of a sorted array, but there is no statement
+  // that the array is sorted, so that would be O(n log n)
+  panic!("No match found");
 }
 
 #[test]
